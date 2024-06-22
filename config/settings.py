4e284b3e0394bdb6f7c73e79742389ac306d4d9a
Dmitry Pavlushin <pavlushindv@gmail.com>
2024-06-22 09:33:56 +0300

    Correct from 22/06/2024

--- config/settings.py
+++ config/settings.py
@@ -13,10 +13,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from pathlib import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 dot_env = os.path.join(BASE_DIR, '.env')
-#load_dotenv(dotenv_path=dot_env)
+load_dotenv(dotenv_path=dot_env)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
